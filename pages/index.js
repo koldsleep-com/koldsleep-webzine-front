c435777d09dcf781729bf99c3b84d1b92a53386b
jiyuunyang <jiyuunyang@gmail.com>
2022-10-03 11:48:00 +0900

    [Init] Initialization

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+const HomePage = () => {
+  return <h1>This is Home</h1>;
+};
+
+export default HomePage;
---
+++ pages/page/[pageId].js
@@ -0,0 +1,5 @@
+const Detail = () => {
+  return <h1>DetailPage</h1>;
+};
+
+export default Detail;
