c5643e5f58d50fea18caa306dace399f5e5719db
jiyuunyang <jiyuunyang@gmail.com>
2022-10-12 23:58:42 +0900

    [Style] Fix Styles

--- components/Divider.js
+++ components/Divider.js
@@ -5,10 +5,17 @@ const Divider = () => {
   return (
     <div className='divider--wrapper'>
       <div className='divider--side'></div>
-      <img alt='star' src='/assets/ico_star_+.svg' />
+      <img className='divider--img' alt='star' src='/assets/ico_star_+.svg' />
       <div className='divider--center'></div>
-      <img alt='star' src='/assets/ico_star_+.svg' />
+      <img className='divider--img' alt='star' src='/assets/ico_star_+.svg' />
       <div className='divider--side'></div>
+      <div className='divider--side--mb'></div>
+      <img
+        className='divider--img--mb'
+        alt='star'
+        src='/assets/ico_star_+.svg'
+      />
+      <div className='divider--side--mb'></div>
     </div>
   );
 };
