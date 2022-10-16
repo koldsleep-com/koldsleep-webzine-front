9a719ccf968a4a1765a21dca7698d821dad53de3
jiyuunyang <jiyuunyang@gmail.com>
2022-10-17 00:11:25 +0900

    [Add] Add header wrapper, fix styles

--- components/Header.js
+++ components/Header.js
@@ -5,18 +5,25 @@ const Header = () => {
     <header>
       <div className='header__side'>! GO KOLDSLEEP</div>
       <div className='header__logo'>
-        <span>해피구독</span>
+        <span className='header__logo--happysubscribe'>해피구독</span>
         <Image
+          className='header__logo--x'
           alt='pin'
           src='/assets/ico_star_x.svg'
           width='13px'
           height='13px'
         />{' '}
-        <span>HAPPY</span>
-        <img alt='pin' src='/assets/ico_star_+.svg' className='save' />{' '}
-        <span>SAVE</span>
-        <img alt='pin' src='/assets/ico_star_+.svg' className='project' />{' '}
-        <span>PROJECT</span>
+        <span className='header__logo--happysaveproject'>
+          <span>HAPPY</span>
+          <img alt='pin' src='/assets/ico_star_+.svg' className='save' />{' '}
+          <span>SAVE</span>
+          <img
+            alt='pin'
+            src='/assets/ico_star_+.svg'
+            className='project'
+          />{' '}
+          <span>PROJECT</span>
+        </span>
       </div>
       <div className='header__side'></div>
     </header>
