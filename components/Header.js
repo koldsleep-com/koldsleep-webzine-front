944da34025352018689a7a4c3b1536feb69bb7bf
jiyuunyang <jiyuunyang@gmail.com>
2022-11-03 21:28:54 +0900

    [Add] Add Links

--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,35 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header>
-      <div className='header__side'>! GO KOLDSLEEP</div>
-      <div className='header__logo'>
-        <span className='header__logo--happysubscribe'>해피구독</span>
-        <Image
-          className='header__logo--x'
-          alt='pin'
-          src='/assets/ico_star_x.svg'
-          width='13px'
-          height='13px'
-        />{' '}
-        <span className='header__logo--happysaveproject'>
-          <span>HAPPY</span>
-          <img alt='pin' src='/assets/ico_star_+.svg' className='save' />{' '}
-          <span>SAVE</span>
-          <img
+      <Link href='https://www.koldsleep.com'>
+        <div className='header__side'>! GO KOLDSLEEP</div>
+      </Link>
+      <Link href='/'>
+        <div className='header__logo'>
+          <span className='header__logo--happysubscribe'>해피구독</span>
+          <Image
+            className='header__logo--x'
             alt='pin'
-            src='/assets/ico_star_+.svg'
-            className='project'
+            src='/assets/ico_star_x.svg'
+            width='13px'
+            height='13px'
           />{' '}
-          <span>PROJECT</span>
-        </span>
-      </div>
+          <span className='header__logo--happysaveproject'>
+            <span>HAPPY</span>
+            <img alt='pin' src='/assets/ico_star_+.svg' className='save' />{' '}
+            <span>SAVE</span>
+            <img
+              alt='pin'
+              src='/assets/ico_star_+.svg'
+              className='project'
+            />{' '}
+            <span>PROJECT</span>
+          </span>
+        </div>
+      </Link>
       <div className='header__side'></div>
     </header>
   );
